aa1190d4b8d69e624b115a0b92389e64701d0063
Angel Lugo <this@angellugo.com>
2018-07-11 14:53:25 -0700

    Added the spotif funtionality and fixed the twiter lookup number of tweets

--- liri.js
+++ liri.js
@@ -1,48 +1,54 @@
+const debug = false;
+
 // Read and set environment variables
-require("dotenv").config();
+require ('dotenv').config ();
 
-console.log("Loading keys.js...");
-var keys = require("./keys");
+debug && console.log ('Loading keys.js...');
+const keys = require ('./keys');
 
+process.argv[2] === 'my-tweets' && getTweets (process.argv[3]);
+process.argv[2] === 'spotify-this-song' && getSong (process.argv[3]);
 
-process.argv[2] === "my-tweets" && getTweets(process.argv[3]);
+function getTweets (screenName) {
+  console.log ('\nLoading tweets...');
 
-
-
-
-function getTweets(screenName) {
-  console.log("\nLoading tweets...");
-
-  var Twitter = require('twitter'); 
-  var twitter = new Twitter(keys.twitter);
-  var params = { screen_name: screenName || 'nodejs', count: 2 };
-  twitter.get('statuses/user_timeline', params, function (error, tweets, response) {
+  const Twitter = require ('twitter');
+  const twitter = new Twitter (keys.twitter);
+  const params = {screen_name: screenName || 'nodejs', count: 20};
+  twitter.get ('statuses/user_timeline', params, function (
+    error,
+    tweets,
+    response
+  ) {
     if (error) {
-      console.log(error);
-    }
-    else {
-      tweets.forEach(tweet => {
-        console.log("\n" + tweet.created_at + " - " + tweet.text);
+      console.log (error);
+    } else {
+      tweets.forEach (tweet => {
+        console.log ('\n' + tweet.created_at + ' - ' + tweet.text);
       });
     }
   });
 }
-// var Spotify = require('node-spotify-api');
-// var spotify = new Spotify(keys.spotify);
 
-// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-//   if (err) {
-//     return console.log('Error occurred: ' + err);
-//   }
- 
-// console.log(data); 
-// });
+function getSong (song) {
+  var songName = song || 'The Sign artist:Ace of Base';
+  debug && console.log ('songName', songName);
+  
+  console.log ('\nLoading Spotify data...');
+  const Spotify = require ('node-spotify-api');
+  const spotify = new Spotify (keys.spotify);
+  const params = {type: 'track', query: "'" + songName + "'", limit: 1};
+  
+  spotify.search (params, function (err, data) {
+    if (err) {
+      return console.log ('Error occurred: ' + err);
+    }
 
-// spotify
-//   .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
-//   .then(function(data) {
-//     console.log(data); 
-//   })
-//   .catch(function(err) {
-//     console.error('Error occurred: ' + err); 
-//   });
+    debug && console.log(data.tracks.items[0]);
+
+    console.log ('Artist:', data.tracks.items[0].album.artists[0].name);
+    console.log ('Song Name:', data.tracks.items[0].name);
+    console.log ('Link to song:', data.tracks.items[0].album.external_urls.spotify);
+    console.log ('Album Name:', data.tracks.items[0].album.name);
+  });
+}
