02c8879c854cefb0ca8d7e30fcd39bbad9c1636d
Angel Lugo <this@angellugo.com>
2018-07-16 12:17:32 -0700

    Added logging to file and formated code

--- liri.js
+++ liri.js
@@ -10,31 +10,48 @@ const fs = require ('fs');
 
 run (process.argv[2], process.argv[3]);
 
-function run (option1, option2) {
-  option1 === 'my-tweets' && getTweets (option2);
-  option1 === 'spotify-this-song' && getSong (option2);
-  option1 === 'movie-this' && getMovie (option2);
-  option1 === 'do-what-it-says' && doWhatItsSays ();
-}
+function run (command, instructions) {
+  log (command, instructions);
+  switch (command) {
+    case 'my-tweets':
+      getTweets (instructions);
+      break;
+    case 'spotify-this-song':
+      getSong (instructions);
+      break;
+    case 'movie-this':
+      getMovie (instructions);
+      break;
+    case 'do-what-it-says':
+      doWhatItsSays ();
+      break;
+    default:
+      console.log ('Invalid input. Please read README for instructions.');
+  }// end switch
+}// end function run
+
+function log (command, instructions) {
+  fs.appendFile ('log.txt', command + ',' + instructions + '\n', function (error) {
+    if (error) {
+      return console.log (error);
+    }
+  });// end fs.appendFile
+}// end function log
 
 function getTweets (screenName) {
   console.log ('\nLoading tweets...');
   const twitter = new Twitter (keys.twitter);
   const params = {screen_name: screenName || 'nodejs', count: 20};
-  twitter.get ('statuses/user_timeline', params, function (
-    error,
-    tweets,
-    response
-  ) {
+  twitter.get ('statuses/user_timeline', params, function (error, tweets, response) {
     if (error) {
       console.log (error);
     } else {
       tweets.forEach (tweet => {
         console.log ('\n' + tweet.created_at + ' - ' + tweet.text);
-      });
-    }
-  });
-}
+      });// end tweets.forEach (tweet => {
+    }// end else
+  });// end twitter.get 
+}// end function getTweets
 
 function getSong (song) {
   var songName = song || 'The Sign artist:Ace of Base';
@@ -43,19 +60,16 @@ function getSong (song) {
   console.log ('\nLoading Spotify data...');
   const spotify = new Spotify (keys.spotify);
   const params = {type: 'track', query: "'" + songName + "'", limit: 1};
-  spotify.search (params, function (err, data) {
-    if (err) {
-      return console.log ('Error occurred: ' + err);
+  spotify.search (params, function (error, data) {
+    if (error) {
+      return console.log ('Error occurred: ' + error);
     }
 
     debug && console.log (data.tracks.items[0]);
 
     console.log ('Artist:', data.tracks.items[0].album.artists[0].name);
     console.log ('Song Name:', data.tracks.items[0].name);
-    console.log (
-      'Link to song:',
-      data.tracks.items[0].album.external_urls.spotify
-    );
+    console.log ('Link to song:', data.tracks.items[0].album.external_urls.spotify);
     console.log ('Album Name:', data.tracks.items[0].album.name);
   });
 }
@@ -63,33 +77,22 @@ function getSong (song) {
 function getMovie (movie) {
   console.log ('\nLoading OMDb data...');
   var movieTitle = movie || 'Mr Nobody';
-  request (
-    'http://www.omdbapi.com/?t=' + movieTitle + '&y=&plot=short&apikey=trilogy',
-    function (error, response, body) {
+  request ('http://www.omdbapi.com/?t=' + movieTitle + '&y=&plot=short&apikey=trilogy', function (error, response, body) {
       if (!error && response.statusCode === 200) {
-        // console.log (body);
+        debug && console.log (body);
         console.log ("The movie's title is: " + JSON.parse (body).Title);
         console.log ('Year the movie was released: ' + JSON.parse (body).Year);
-        console.log (
-          "The movie's IMDb rating is: " + JSON.parse (body).imdbRating
-        );
-        // console.log ("The movie's Rotten Tomatoes rating is: " + JSON.parse (body).tomatoRating);
-        console.log (
-          "The movie's Rotten Tomatoes rating is: " +
-            JSON.parse (body).Ratings[1].Value
-        );
-        console.log (
-          'Country where the movie was produced: ' + JSON.parse (body).Country
-        );
+        console.log ("The movie's IMDb rating is: " + JSON.parse (body).imdbRating);
+        console.log ("The movie's Rotten Tomatoes rating is: " + JSON.parse (body).Ratings[1].Value);
+        console.log ('Country where the movie was produced: ' + JSON.parse (body).Country);
         console.log ('Language of the movie: ' + JSON.parse (body).Language);
         console.log ('Plot of the movie: ' + JSON.parse (body).Plot);
         console.log ('Actors in the movie: ' + JSON.parse (body).Actors);
       } else {
         console.log (error);
-      }
-    }
-  );
-}
+      }//end else
+    });// end request
+}//end function getMovie
 
 function doWhatItsSays () {
   console.log ('\nLoading file data...');
@@ -98,12 +101,12 @@ function doWhatItsSays () {
     if (error) {
       return console.log (error);
     }
-    const lines = data.split ('\r\n'); 
-    debug && console.log('lines', lines);
+    const lines = data.split ('\r\n');
+    debug && console.log ('lines', lines);
     lines.forEach (line => {
-      const lineItems = line.split (','); 
+      const lineItems = line.split (',');
       debug && console.log (lineItems[0], lineItems[1]);
-      setTimeout(function(){ run (lineItems[0], lineItems[1]); }, 3000);
+      run (lineItems[0], lineItems[1]);
     }); // lines.forEach(line => {
   }); // end fs.readFile("random.txt", "utf8", function(error, data) {
-}
+}//end function doWhatItSays
